911bcacd73f6d91bac0aa28357cdda9e75d55318
Jack Leonard <w0499622@nscc.ca>
2024-09-19 09:37:55 -0300

    First commit was an accident left it empty

--- Lab2-HockeyTeam.py
+++ Lab2-HockeyTeam.py
@@ -13,12 +13,14 @@
 def main():
     # YOUR CODE STARTS HERE, each line must be indented (one tab)
 
-
-
-
-
-
-
+    teamName = input("Enter the name a hockey team.")                   #Taking in team name
+    teamWins = int(input("Enter how many wins that team has "))         #wins
+    teamLosses = int(input("Enter how many losses that team has "))     #losses
+    gamesPlayed = teamWins + teamLosses                                 #calculating total games used in next operation
+    winPercentage = f"{((teamWins * 100) / gamesPlayed):.4f}"           #calculating win percentage wins x 100 / games played   it is then turned into an f string so it can be printed without doing so in the print line 
+                            
+    
+    print("The " + teamName + " have " + str(teamWins) + " wins and " + str(teamLosses) + " losses, with a win percentage of "  +  winPercentage)           #print format line turns team wins and team losses into strings using casting     
 
     # YOUR CODE ENDS HERE
 
